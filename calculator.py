f960c84db986694ff1dffa90caccc63dc74cbd50
beginningProgrammer221 <164580786+beginningProgrammer221@users.noreply.github.com>
2025-04-10 17:03:51 -0400

    added square_root and hypotenuse

--- calculator.py
+++ calculator.py
@@ -8,11 +8,19 @@ One function per operation, in order.
 import math
 
 # First example
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    else:
+        if math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
 
 def add(a, b): 
     return a + b
 
-def substract(a, b):
+def subtract(a, b):
     return a - b
 
 def multiply(a, b):
@@ -27,10 +35,12 @@ def divide(b, a):
 
 def logarithm(a, b):
     if a<= 0 or b <= 0 or a == 1:
-        return math.log(a, b)
-    else:
         raise ValueError
+    else:
+        return math.log(a, b)
+
 
 
 def exponent(a, b):
     return a ** b
+
